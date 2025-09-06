94ef4c435d3148ddd161ca30624f7ef49b6ed3f3
KR0079384 <karthiraman2006@gmail.com>
2025-09-06 11:59:01 +0530

    First Commit

---
+++ src/firstComp.tsx
@@ -0,0 +1,9 @@
+export default function FirstComp() {
+  return (
+    <>
+      <div className="bg-yellow-400 p-4 rounded-lg text-black font-bold">
+        Hello World
+      </div>
+    </>
+  );
+}
